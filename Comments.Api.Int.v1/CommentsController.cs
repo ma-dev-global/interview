97423ad505b4699a329b8e551b885a5f1c40c916
dgangler <37552142+dgangler@users.noreply.github.com>
2020-08-10 17:59:20 -0500

    create solution and add controller, service, and db

---
+++ Comments.Api.Int.v1/CommentsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Comments.Model;
+
+namespace Comments.Api.Int.v1
+{
+	[RoutePrefix("api/v1/comment")]
+	public class CommentsController : ApiController {
+
+		private readonly CommentsService commentsSvc;
+
+		[HttpGet]
+		[Route("")]
+		public async Task<IHttpActionResult> GetComments(int userId = 0) {
+			IEnumerable<Comment> commentsTask = await commentsSvc.GetComments(userId);
+
+			return Ok(commentsTask);
+		}
+
+
+
+		[HttpPost]
+		[Route("")]
+		public async Task<IHttpActionResult> SaveComment([FromBody] Comment comment) {
+			Comment commentsTask = await commentsSvc.AddComment(comment);
+
+			return Ok(commentsTask);
+		}
+	}
+}
---
+++ Comments.Model/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comments.Model
+{
+    public class Comment
+    {
+		public int? CommentId { get; set; }
+		public string CommentText { get; set; }
+		public int? UserId { get; set; }
+		public DateTime? CreatedTimestamp { get; set; }
+	}
+}
